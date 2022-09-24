0c33276f2fe0f8e15e23743d47755a437d895546
Rajesh <rajeshbhoye3d@live.com>
2022-09-23 20:23:04 -0700

    Added user profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
@@ -116,3 +118,30 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
 	"""Handle creataing user authentication tokens"""
 	renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+'''class UserProfileFeedViewSet(viewsets.ModelViewSet):
+	"""Handles Craeting ,reading and updating profile feed items"""
+	authentication_classes = (TokenAuthentication,)
+	serializer_class = serializers.ProfileFeedItemSerializer
+	queryset = models.ProfileFeedItem.objects.all()
+
+
+	def perform_create(self,serializer):
+		"""Sets the user profile to the logged in user"""
+		serializer.save(user_profile=self.request.user)
+'''
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+    	permissions.UpdateOwnStatus,
+#    	IsAuthenticatedOrReadOnly
+		IsAuthenticated
+    	)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
